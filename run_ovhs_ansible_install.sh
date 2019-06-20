2255df985148543731ab7417498bc3b198ce8f63
root <root@vps342453.ovh.net>
2019-06-20 13:59:08 +0200

    geändert:   run_ovhs_ansible_install.sh

--- run_ovhs_ansible_install.sh
+++ run_ovhs_ansible_install.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
-pwd
-
-test -d roles || exit
-
 ssh-keygen
 #ssh-copy-id root@localhost
 
+rpm -q git || yum install -y git && {
+  git clone https://github.com/joschro/joschro.git
+  git clone https://github.com/joschro/ovhs.git
+}
+
+rpm -q ansible || yum install -y ansible && {
+  echo ansible-playbook 01_ovirt.yml
+}
 #ansible-galaxy install -r roles/requirements.yml -p roles
-rpm -q ansible || yum install -y ansible
-ansible-playbook 01_ovirt.yml
