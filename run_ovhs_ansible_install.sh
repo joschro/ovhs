baf7ab4923e809a8b4f9bf429d3d7c74360e8a67
root <root@ovhs.local>
2019-06-20 19:14:54 +0200

    neue Datei:   defaults/main.yml 	geändert:   hosts 	gelöscht:    inventory/hosts 	geändert:   run_ovhs_ansible_install.sh

--- run_ovhs_ansible_install.sh
+++ run_ovhs_ansible_install.sh
@@ -6,6 +6,17 @@ ssh-keygen
 rpm -q git || yum install -y git && {
   git clone https://github.com/joschro/joschro.git
   git clone https://github.com/joschro/ovhs.git
+  curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+  cat <<EOF > ~/.vimrc
+" Plugins will be downloaded under the specified directory.
+call plug#begin('~/.vim/plugged')
+
+" Declare the list of plugins.
+Plug 'pearofducks/ansible-vim'
+
+" List ends here. Plugins become visible to Vim after this call.
+call plug#end()
+EOF
 }
 
 rpm -q ansible || yum install -y ansible && {
