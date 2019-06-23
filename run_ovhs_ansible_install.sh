499ccfe757e6b2f16625525c72705190654208d1
root <root@ovhs01.ovhs.home>
2019-06-24 00:25:07 +0200

    geändert:   01_ovhs_base_setup.yml

--- run_ovhs_ansible_install.sh
+++ run_ovhs_ansible_install.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 test -f ~/.ssh/id_rsa || ssh-keygen
-#ssh-copy-id root@localhost
 
 cd ~
 rpm -q git || yum install -y git
@@ -11,9 +10,11 @@ test -f ovhs/run_ovhs_ansible_install.sh || git clone https://github.com/joschro
 cd ovhs
 rpm -q ansible || yum install -y ansible
 #ansible-playbook --vault-password-file ../vault_pass -i inventory/hosts --check 01_ovhs_base_setup.yml
-ansible-playbook --vault-password-file ../vault_pass -i inventory/hosts $* 01_ovhs_base_setup.yml
+ansible-playbook --vault-password-file ../vault_pass -i inventory/hosts -t networking $* 01_ovhs_base_setup.yml
+echo;echo "$0 done with networking part. Press any key."
+read ANSW
+ssh-copy-id root@ovhs01_back
+ansible-playbook --vault-password-file ../vault_pass -i inventory/hosts -t storage $* 01_ovhs_base_setup.yml
+
 
 #ansible-galaxy install -r roles/requirements.yml -p roles
-
-echo;echo "$0 done. Press any key."
-read ANSW
