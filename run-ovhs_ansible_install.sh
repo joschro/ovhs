312305a5bfeec8cfbc048a827dc38f8a012c1b8b
root <jo@joschro.de>
2020-01-03 22:44:28 +0100

    geändert:   01-ovhs_base_setup.yml 	geändert:   ansible.cfg 	umbenannt:    inventory/group_vars/all/vault.yml -> inventory/group_vars/all/vault.template 	geändert:   inventory/group_vars/all/vault.yml 	geändert:   inventory/host_vars/gluster.yml 	neue Datei:   roles/ovhs.dnsmasq/defaults/main.yml 	neue Datei:   roles/ovhs.dnsmasq/handlers/main.yml 	neue Datei:   roles/ovhs.dnsmasq/meta/main.yml 	neue Datei:   roles/ovhs.dnsmasq/tasks/configuration.yml 	neue Datei:   roles/ovhs.dnsmasq/tasks/installation.yml 	neue Datei:   roles/ovhs.dnsmasq/tasks/main.yml 	neue Datei:   roles/ovhs.dnsmasq/templates/etc/dnsmasq.d/dnsmasq-ovhs.conf.j2 	neue Datei:   roles/ovhs.dnsmasq/vars/Archlinux.yml 	neue Datei:   roles/ovhs.dnsmasq/vars/Debian.yml 	neue Datei:   roles/ovhs.dnsmasq/vars/RedHat.yml 	geändert:   roles/ovhs.etc_hosts/tasks/configuration.yml 	geändert:   run-ovhs_ansible_install.sh

--- run-ovhs_ansible_install.sh
+++ run-ovhs_ansible_install.sh
@@ -3,7 +3,8 @@
 test -f ~/.ssh/id_rsa || ssh-keygen
 
 cd ~
-rpm -q git || yum install -y git
+rpm -q git || yum install -y git ansible
+rpm -q ansible || yum install -y ansible
 test -f joschro/git-update.sh || git clone https://github.com/joschro/joschro.git
 test -f ovhs/run-ovhs_ansible_install.sh || {
   git clone https://github.com/joschro/ovhs.git
@@ -11,8 +12,9 @@ test -f ovhs/run-ovhs_ansible_install.sh || {
   exit
 }
 
+test -d /etc/ansible/roles/ovirt.repositories || ansible-galaxy install ovirt.repositories
+test -d /etc/ansible/roles/ovirt.engine-setup || ansible-galaxy install ovirt.engine-setup
 cd ovhs
-rpm -q ansible || yum install -y ansible
 #ansible-playbook --vault-password-file ../vault_pass -i inventory/hosts --check 01-ovhs_base_setup.yml
 ansible-playbook --ask-vault-pass -i inventory/hosts -t networking $* 01-ovhs_base_setup.yml
 echo;echo "$0 done with networking part. Press any key."
